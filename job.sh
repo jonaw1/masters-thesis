d8358441df4b2e7b78db360ba353e11de2a511e5
jonaw1 <wagnerxjonathan@gmail.com>
2024-10-30 10:33:09 +0100

    intial commit

---
+++ job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#SBATCH --time=01:00:00
+#SBATCH --output=example.out
+#SBATCH --error=error.out
+#SBATCH --mem=20000M
+#SBATCH --gpus=1
+
+module purge
+module load cuda/11.7
+source venv/bin/activate
+python3 main.py
