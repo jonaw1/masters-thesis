88256b1ab4c7f97f699dc927e49dcaf4a65706a5
jonaw1 <wagnerxjonathan@gmail.com>
2024-11-04 15:02:37 +0100

    fix automodel

--- src/modules/setup.py
+++ src/modules/setup.py
@@ -1,4 +1,4 @@
-from transformers import AutoModel, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
 GPT2_XL_MODEL_NAME = "gpt2-xl"
@@ -6,7 +6,7 @@ GPT_J_MODEL_NAME = "EleutherAI/gpt-j-6B"
 
 
 def get_model_tokenizer_device(model_name):
-    model = AutoModel.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
